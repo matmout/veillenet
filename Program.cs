0105f9cefa95a91189fa75c955648544c97c4dcc
matmout <matthieu.trachsel@gmail.com>
2025-12-06 12:21:41 +0100

    Update

--- Program.cs
+++ Program.cs
@@ -26,15 +26,21 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
 
 // Activer les fichiers statiques (nécessaire pour wwwroot)
 app.UseStaticFiles();
+
+app.UseHttpsRedirection();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapRazorPages().WithStaticAssets();
 
 app.Run();
